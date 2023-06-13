ce49483b3d3502e1fe112474cf7aa0ab554c945f
carloeusebi <carloeuse@gmail.com>
2023-06-13 13:21:24 +0200

    Completed gatsby snack

--- js/gatsby.js
+++ js/gatsby.js
@@ -1,1 +1,13 @@
-console.log('GATSBY');
+const table = 'Tavolo Vip';
+const guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
+const guestObjects = guests.map((guest, index) => {
+    place = index + 1;
+    return {
+        name: guest,
+        table: table,
+        place: place
+    };
+});
+
+console.log(guestObjects);
