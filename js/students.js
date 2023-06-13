bf40a47b228d73ffd6f2e9664dc119a4161bea5d
carloeusebi <carloeuse@gmail.com>
2023-06-13 13:24:48 +0200

    Created students' students

--- js/students.js
+++ js/students.js
@@ -1,1 +1,39 @@
-console.log('STUDENTS');
+const students = [
+    {
+        id: 213,
+        name: 'Marco della Rovere',
+        grades: 78
+    },
+    {
+        id: 110,
+        name: 'Paolo Cortallessa',
+        grades: 96
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grades: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grades: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades: 68
+    },
+    {
+        id: 102,
+        name: 'Piero Della Francesca',
+        grades: 50
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grades: 84
+    }
+];
+
+console.log(students);
