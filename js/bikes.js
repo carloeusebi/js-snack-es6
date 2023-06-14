367d13c1d6e58f73db9861382c711ce61116cd3b
carloeusebi <carloeuse@gmail.com>
2023-06-14 16:04:06 +0200

    Printed lightest bike on html

--- js/bikes.js
+++ js/bikes.js
@@ -17,7 +17,7 @@ console.log('--------------------------------------');
 
 /**
  * Given an array of objects and a property it returns the object with the lowest value in that property
- * @param {object} array the array of objects to search into
+ * @param {[object]} array the array of objects to search into
  * @param {string} property the property to evaluate
  * @returns {object} the object with the lowest value property
  */
@@ -25,8 +25,13 @@ const getLowestValueItem = (array, property) => {
     return array.reduce((lowest, arr) => arr[property] < lowest[property] ? arr : lowest, array[0]);
 }
 
-lighterBike = getLowestValueItem(bikes, 'weight');
+const lightestBike = getLowestValueItem(bikes, 'weight');
 
-console.log('Lighter Bike:');
-console.log(lighterBike);
+console.log('Lightest Bike:');
+console.log(lightestBike);
 
+// print on page
+
+const { name: bikeName, weight } = lightestBike;
+
+document.body.innerHTML += `The lightest bike is ${bikeName} and it weights ${weight}kg.`;
